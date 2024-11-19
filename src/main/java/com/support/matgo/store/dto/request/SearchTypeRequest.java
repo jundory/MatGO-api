4c4eadc25ba8ecb3e90e703052545060d1bf1c0c
jundory <ykwanlee98@naver.com>
2024-11-19 11:19:57 +0900

    Feat: 검색 필터 쿼리 문제 수정 및 조건 변경

--- src/main/java/com/support/matgo/store/dto/request/SearchTypeRequest.java
+++ src/main/java/com/support/matgo/store/dto/request/SearchTypeRequest.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Getter
+@Setter
 public class SearchTypeRequest extends CoordinateRequest{
   private List<String> member;
   private List<String> food;
