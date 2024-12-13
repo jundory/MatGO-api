2795f864db4a304f01adbbd2283334cc3e6d787e
jundory <ykwanlee98@naver.com>
2024-12-13 14:09:27 +0900

    Feat: 상세 화면 예외처리 추가 및 주석 메모

--- src/main/java/com/support/matgo/store/service/StoreService.java
+++ src/main/java/com/support/matgo/store/service/StoreService.java
@@ -75,6 +75,7 @@ public class StoreService {
 
   /* 가게 상세 정보 조회 메소드 */
   public DetailApiResponse getStoreInfo(String storeId){
+    try {
       // 1. img 메타 데이터 조회 & imgUrl만 별도 리스트로 생성
       List<ImageInfo> imageInfoList = imageRepository.findByStoreId(storeId);
 
@@ -85,9 +86,18 @@ public class StoreService {
 
       // 2. 가게 상세 정보 조회
       StoreInfo storeDetailInfo = storeRepository.findByStoreId(storeId);
+      // null 체크
       if (storeDetailInfo == null) {
-        throw new CustomException(ErrorCode.STORE_NOT_FOUND);
+        throw new CustomException(ErrorCode.DETAIL_NO_CONTENT);
       }
+      /**
+       * - 상세정보가 없는 예외처리가 필요할까? ←리스트에서 클릭해서 들어왔는데, 상세정보가 없다고 나온다면 이건 분명한 오류로 봐야 함.(이미지가 없을 순 있어도 최소한의 정보는 필요함 ← 프로젝트의 존재의의)
+       * - 즉, 리스트에 표출되는 가게는 무조건 상세정보가 있어야 함 → 크롤링 시점에서 두 컬렉션에 같은 storeId 키를 가진 가게가 존재해야 함
+       * - 리스트와 상세 페이지 모두 storId를 키로 가지고 있을꺼고, 클릭해서 상세 화면으로 이동 시 잘못된 storeId가 전달될 일도 없음. (이미 리스트에 표출된 가게를 클릭해서 들어올 것이기 때문)
+       * - 이미지는 없을 시 자동으로 null 처리되어 예외처리 필요x
+       * 결론) 상세 화면의 storeDetailInfo에 대한 예외처리는 필요 없을 것으로 보임. (이미지는 없으면 없는 대로, 상세는 없으면 안되기 때문에)
+       */
+
       StoreDetailInfoResponse storeData = StoreDetailInfoResponse.builder()
           .storeInfo(new StoreInfoResponse(storeDetailInfo))  //entity to dto
           .imgList(imgList)
@@ -99,6 +109,11 @@ public class StoreService {
           .result(storeData)
           .build();
       return result;
+    } catch (CustomException e){
+      throw e;
+    } catch (Exception e){
+      throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
   }
 
   /* Sorting & Entity to DTO */
