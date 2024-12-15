1f315700ae8cc3279b995f1c799e50c4795673bd
jundory <ykwanlee98@naver.com>
2024-12-15 17:23:50 +0900

    Feat: corsConfig 추가 및 Securiry 의존성 추가 후 Config 분리

---
+++ src/main/java/com/support/matgo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.support.matgo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+  private final CorsConfig corsConfig;
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())   //csrf 비활성화
+//        .cors(cors -> cors.disable())   //cors 비활성화
+        .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource())) // CORS 설정
+//        .addFilter(corsConfig.corsFilter())
+        .authorizeRequests(authorize -> authorize
+                .requestMatchers("/**").permitAll() // 요청 허용할 경로 명시(현재는 모든 경로)
+//                        .anyRequest().authenticated() // 밑의 permitAll()과 반대로 인증할 경로 명시(인증된 사용자만 접근 허용)
+        );
+    return http.build();
+  }
+}
